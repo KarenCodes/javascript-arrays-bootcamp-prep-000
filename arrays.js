83e83d4049cc04edda3fec6e3c249492251ef01b
Karen Neumann <karen@websavvystudio.com>
2018-01-18 15:53:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,6 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array,element]
+  return newArray
+}
+addElementToEndOfArray()
