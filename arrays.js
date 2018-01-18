4d147e29bc1f982d7d6219d3aa5a047ce9f5f017
Karen Neumann <karen@websavvystudio.com>
2018-01-18 15:54:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,3 +5,9 @@ function addElementToEndOfArray(array, element) {
   return newArray
 }
 addElementToEndOfArray()
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array,element]
+  return newArray
+}
+addElementToEndOfArray()
