3f5340ff1f6e490c014f7374c1e8e196c4d5f0f9
Karen Neumann <karen@websavvystudio.com>
2018-01-18 16:01:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,25 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element,...array]
+  return newArray
+}
+addElementToBeginningOfArray()
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = [element...,array]
+  return array
+}
+destructivelyAddElementToBeginningOfArray()
+
 function addElementToEndOfArray(array, element) {
-  var newArray = [...array,element]
+  var newArray = [...array, element]
   return newArray
 }
 addElementToEndOfArray()
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  array = [...array,element]
+function destructivelyAddElementToEndOfArray(array, element) {
+  array = [array, element]
   return array
 }
-destructivelyAddElementToBeginningOfArray()
+destructivelyAddElementToEndOfArray()
